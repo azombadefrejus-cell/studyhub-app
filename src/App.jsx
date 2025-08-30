9b6e1aafee8ff2cb125bdd4e37a6c29f7675f7d7
frejus <azombadefrejus@gmail.com>
2025-08-30 17:53:55 +0100

    premier commit du projet

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { io } from "socket.io-client";
 
 // --- Configuration ---
-const API_URL = 'http://localhost:3001';
+const API_URL = 'https://studyhub-server-frejus.onrender.com'
+
+
 
 // --- Icônes (Composants SVG) ---
 const HomeIcon = (props) => <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>;
